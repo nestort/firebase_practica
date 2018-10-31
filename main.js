da49b2562d42182287b9852be45f56d444260ab2
nestort <nestor.t.1992@gmail.com>
2018-10-30 20:40:42 -0600

    faltaba() ..XD

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var provider = new firebase.auth.GoogleAuthProvider();
 $('#inicioSesion').click(
     
     function(result){
-        //firebase.auth.signInWithPopup(provider).then(
+        //firebase.auth.signInWithPopup(provider).then(    <=faltaba ()
         firebase.auth().signInWithPopup(provider).then(
         
             function(result){
