994c709a2906e117ac394759b48a9837026b2876
nestort <nestor.t.1992@gmail.com>
2018-10-30 20:40:03 -0600

    faltaba ()

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-var provedor= new firebase.auth.GoogleAuthProvider();
-$('#inicioSesion').click();
+var provider = new firebase.auth.GoogleAuthProvider();
+
+$('#inicioSesion').click(
+    
+    function(result){
+        //firebase.auth.signInWithPopup(provider).then(
+        firebase.auth().signInWithPopup(provider).then(
+        
+            function(result){
+                console.log(result);
+            }
+
+
+        );
+    }
+);
